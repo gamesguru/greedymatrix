4ff8cac5acf819eb1c93f59bae69d638d7755c63
gamesguru <mathmuncher11@gmail.com>
2020-01-17 20:12:25 -0500

    add 21x21

--- miss-gen.py
+++ miss-gen.py
@@ -14,10 +14,9 @@ matrix = np.ones((n, n), dtype=np.byte,)
 # Inject missing values
 for i in range(0, n):
     for j in range(0, n):
-        if random() > missing_perc:
-            matrix[i][j] = 1
-        else:
+        if random() < missing_perc:
             matrix[i][j] = 0
+            matrix[j][i] = 0
 
 # Write to CSV file
 np.savetxt(f'{n}x{n}.csv', matrix, fmt='%i', delimiter=',')

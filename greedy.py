7ce0816690ec1649f807c3d24dfc7f1108a658b2
gamesguru <mathmuncher11@gmail.com>
2020-01-17 23:23:12 -0500

    tweak greedy.py and small.csv

--- greedy.py
+++ greedy.py
@@ -6,7 +6,9 @@ import csv
 nums = set()
 pairs = set()
 
-with open("large.csv") as f:
+print("Read in CSV")
+# Read in
+with open("small.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         i = int(row[2])
@@ -19,39 +21,40 @@ with open("large.csv") as f:
 n = max(nums)
 print(n)
 
-print('Prune compliments')
+print("Prune compliments")
 # Remove compliments
 for i in range(0, n):
     for j in range(0, n):
         pair = (i, j)
         anti_pair = (j, i)
         if not pair in pairs and anti_pair in pairs:
-            print(anti_pair)
             pairs.remove(anti_pair)
+
 print(len(pairs))
 
 matches = {0: 0}
 # Apply Greedy
 while len(matches) > max(matches.values()):
     matches = {}
-    
-    print(f'Tallies: {len(matches)} remaining')
+
+    print(f"\nIterate..")
     for pair in pairs:
         k = pair[0]
         v = pair[1]
         if not k in matches:
             matches[k] = 0
         matches[k] += 1
-
-    print('Find least paired')
+    print(f'{len(matches)} stop remaining')
+    # print("Find least paired")
     k = min(matches, key=matches.get)
     v = matches[k]
     floor = v
-    print('Floor: {floor}')
+    # print(f"Floor: {floor}")
     while matches[k] == floor:
-        print(f'del matches[{k}]= {matches[k]}')
+        print(f"  del {k}->{matches[k]}")
         del matches[k]
         k = min(matches, key=matches.get)
         v = matches[k]
+    print(f'..done!  {len(matches)} remaining')
 
-print(matches.keys())
+print(list(matches.keys()))

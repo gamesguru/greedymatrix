af4457b42d19b4d14f4fd1ac5e7d3b9b3de8b660
gamesguru <mathmuncher11@gmail.com>
2020-01-18 10:19:38 -0500

    mod greedy, add Kyle's paths.js

--- greedy.py
+++ greedy.py
@@ -32,32 +32,47 @@ for i in range(0, n):
 
 print(len(pairs))
 
-matches = {0: 0}
+matches = {i: 0 for i in range(min(nums), n)}
 # Apply Greedy
-while len(matches) > min(matches.values()):
-    print(f'[CONTINUE] {len(matches)} > {min(matches.values())}')
-    matches = {}
+# while len(matches) > min(matches.values()):
+while True:
 
+    print(f"[CONTINUE] {len(matches)} > {min(matches.values())}")
+    matches = {i: 0 for i in matches.keys()}
     print(f"\nIterate..")
     for pair in pairs:
         k = pair[0]
         v = pair[1]
-        if not k in matches:
-            matches[k] = 0
-        matches[k] += 1
+        # Tally matches
+        if k in matches and v in matches:
+            matches[k] += 1
+    
     print(f"{len(matches)} stops remaining")
 
     k = min(matches, key=matches.get)
     v = matches[k]
     floor = v
 
-    print('Prune weakest links')
+    print("Prune weakest links")
     while matches[k] == floor:
         print(f"  del {k}  ({matches[k]} occurances)")
         del matches[k]
         k = min(matches, key=matches.get)
         v = matches[k]
 
+    # Re-iterate
+    matches = {i: 0 for i in matches.keys()}
+    print(f"\nRe-iterate..")
+    for pair in pairs:
+        k = pair[0]
+        v = pair[1]
+        # Tally matches
+        if k in matches and v in matches:
+            matches[k] += 1
+
     print(f"..done!  {len(matches)} remaining")
+    # TODO - retally..
+    if len(matches) > min(matches.values()):
+        break
 
 print(list(matches.keys()))

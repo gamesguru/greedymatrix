a50546f0b2af344d3de006f64e4956bbcaa26db6
gamesguru <mathmuncher11@gmail.com>
2020-01-18 12:47:07 -0500

    give cmd line args, enhance, fix

--- greedy.py
+++ greedy.py
@@ -1,122 +1,127 @@
 #!/usr/bin/env python3
 
+import sys
 import csv
 
 
-# Get pairs and singles
-nums = set()
-pairs = set()
+def main(args):
 
-input_file = '300,300.csv'
-o_index = 0
-d_index = 1
+    # Get pairs and singles
+    nums = set()
+    pairs = set()
 
-print("Read in CSV")
-# Read in
-with open(input_file) as f:
-    reader = csv.reader(f)
-    for row in reader:
-        i = int(row[o_index])
-        j = int(row[d_index])
-        pairs.add((i, j))
-        nums.add(i)
-        nums.add(j)
-
-
-# Find max
-n = len(nums)
-m = max(nums)
-print(f'''Pre-pruning
-Span:  {n}
-Max:   {m}
-Miss:  {[x for x in range(1, m + 1) if not x in nums]}
-Pairs: {len(pairs)}
-''')
-print(f"Num stops: {n}\n")
-
-
-print("Prune compliments")
-# Remove compliments
-for i in nums:
-    for j in nums:
-        pair = (i, j)
-        anti_pair = (j, i)
-        if not pair in pairs and anti_pair in pairs:
-            pairs.remove(anti_pair)
-
-print(f"Post pruning: {len(pairs)} entries")
-print(
-    """
---------------------
-Begin GREEDY algo
---------------------
-"""
-)
-
-# Init dict
-matches = {i: 0 for i in nums}
-
-# Apply Greedy
-loops = 1
-while True:
-
-    print(f"\n\n==> ITERATION #{loops}")
-    # Restart tallies
-    matches = {i: 0 for i in matches.keys()}
-
-    # Tallies
-    for i, j in pairs:
-        if i in matches and j in matches:
-            matches[i] += 1
-    print(f"{len(matches)} stops remaining")
-
-    k = min(matches, key=matches.get)
-    v = matches[k]
-    floor = v
-
-    # Break if square
-    if all(x == floor for x in matches.values()):
-        print(
-            f"""
-----------------------------------
-DONE: all have {floor} matches!
-----------------------------------
-"""
-        )
-        break
-
-    print("Prune weakest links")
-    # Greedy selection
-    while matches[k] == floor:
-        print(f"  del {k}  ({matches[k]} occurances)")
-        del matches[k]
-        k = min(matches, key=matches.get)
-        v = matches[k]
-
-    # Continue
-    loops += 1
-
-
-solution = set(matches.keys())
-print(solution)
-
-
-print('''
-----------------------------------
-Streaming new filtered CSV
-----------------------------------
-''')
-# Stream output
-with open('filtered.csv', 'w+') as csv_out:
-    writer = csv.writer(csv_out)
+    if len(args) > 0:
+        input_file = args[0]
+        o_index = int(args[1])
+        d_index = int(args[2])
+    else:
+        input_file = 'resources/300,300.csv'
+        o_index = 0
+        d_index = 1
 
     # Read in
-    with open(input_file) as csv_in:
-        reader = csv.reader(csv_in)
-
+    print("Read in CSV")
+    with open(input_file) as f:
+        reader = csv.reader(f)
         for row in reader:
             i = int(row[o_index])
             j = int(row[d_index])
-            # Add only members of the square solution
-            if i in solution and j in solution and i != j:
-                writer.writerow(row)
+            pairs.add((i, j))
+            nums.add(i)
+            nums.add(j)
+
+    # Find max
+    n = len(nums)
+    m = max(nums)
+    print(f'''Pre-pruning
+    Span:  {n}
+    Max:   {m}
+    Miss:  {[x for x in range(0, m) if not x in nums]}
+    Pairs: {len(pairs)}
+    ''')
+    print(f"Num stops: {n}\n")
+
+    # Remove compliments
+    print("Prune compliments")
+    for i in nums:
+        for j in nums:
+            pair = (i, j)
+            anti_pair = (j, i)
+            if not pair in pairs and anti_pair in pairs:
+                pairs.remove(anti_pair)
+
+    print(f"Post pruning: {len(pairs)} entries")
+    print(
+        """
+--------------------
+Begin GREEDY algo
+--------------------
+    """
+    )
+
+    # Init dict
+    matches = {i: 0 for i in nums}
+
+    # Apply Greedy
+    loops = 1
+    while True:
+
+        # Restart tallies
+        print(f"\n\n==> ITERATION #{loops}")
+        matches = {i: 0 for i in matches.keys()}
+
+        # Tallies
+        for i, j in pairs:
+            if i in matches and j in matches:
+                matches[i] += 1
+        print(f"{len(matches)} stops remaining")
+
+        k = min(matches, key=matches.get)
+        v = matches[k]
+        floor = v
+
+        # Break if square
+        if all(x == floor for x in matches.values()):
+            print(
+                f"""
+----------------------------------
+DONE: all have {floor} matches!
+----------------------------------
+    """
+            )
+            break
+
+        # Greedy selection
+        print("Prune weakest links")
+        while matches[k] == floor:
+            print(f"  del {k}  ({matches[k]} occurances)")
+            del matches[k]
+            k = min(matches, key=matches.get)
+            v = matches[k]
+
+        # Continue
+        loops += 1
+
+    solution = set(matches.keys())
+    print(solution)
+
+    # Stream output
+    print('\n==> Streaming new filtered CSV')
+    with open(args[0].replace('.csv', '.filtered.csv'), 'w+') as csv_out:
+        writer = csv.writer(csv_out)
+
+        # Read in
+        with open(input_file) as csv_in:
+            reader = csv.reader(csv_in)
+
+            for row in reader:
+                i = int(row[o_index])
+                j = int(row[d_index])
+                # Add only members of the square solution
+                if i in solution and j in solution and i != j:
+                    writer.writerow(row)
+
+
+# Main script executable
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- miss-gen.py
+++ miss-gen.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
 
-import json
+import sys
 from random import random
 
 import numpy as np
 
-# Create the matrix
-n = 300
-missing_perc = 0.04
 
-matrix = np.ones((n, n), dtype=np.byte,)
+def main(args):
+    """ Generates relative i,j CSV format with missing entirs, like ESRI/Arc """
 
-# Inject missing values
-for i in range(0, n):
-    for j in range(0, n):
-        if random() < missing_perc:
-            matrix[i][j] = 0
-            # matrix[j][i] = 0
+    if len(args) > 0:
+        n = int(args[0])
+        missing_perc = float(args[1])
+    else:
+        n = 300
+        missing_perc = 0.04
 
-# Write to CSV file
-np.savetxt(f'{n}x{n}.csv', matrix, fmt='%i', delimiter=',')
+    # Create the matrix
+    matrix = np.ones((n, n), dtype=np.byte,)
+
+    # Inject missing values
+    for i in range(0, n):
+        for j in range(0, n):
+            if random() < missing_perc:
+                matrix[i][j] = 0
+                # matrix[j][i] = 0
+
+    # Write to CSV file
+    np.savetxt(f'resources/_{n}x{n}-{missing_perc}.csv', matrix, fmt='%i', delimiter=',')
+
+    # Auxilary format
+    rows = []
+    for i in range(0, n):
+        for j in range(0, n):
+            if random() > missing_perc:
+                rows.append(f'{i},{j}')
+
+    # with open(f'resources/{n},{n}.csv', 'w+') as f:
+    with open(f'resources/{n}-{missing_perc}.csv', 'w+') as f:
+        for row in rows:
+            f.writelines(row + '\n')
 
 
-# Auxilary format
-rows = []
-for i in range(0, n):
-    for j in range(0, n):
-        if random() > missing_perc:
-            rows.append(f'{i},{j}')
-
-with open(f'{n},{n}.csv', 'w+') as f:
-    for row in rows:
-        f.writelines(row + '\n')
+# Main script executable
+if __name__ == '__main__':
+    main(sys.argv[1:])

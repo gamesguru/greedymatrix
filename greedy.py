71b03844cea6e787a671aaba97e76896c7abd697
gamesguru <mathmuncher11@gmail.com>
2020-01-17 23:16:26 -0500

    debug greedy

--- greedy.py
+++ greedy.py
@@ -6,7 +6,7 @@ import csv
 nums = set()
 pairs = set()
 
-with open("small.csv") as f:
+with open("large.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         i = int(row[2])
@@ -17,7 +17,9 @@ with open("small.csv") as f:
 
 # Find max
 n = max(nums)
+print(n)
 
+print('Prune compliments')
 # Remove compliments
 for i in range(0, n):
     for j in range(0, n):
@@ -26,11 +28,14 @@ for i in range(0, n):
         if not pair in pairs and anti_pair in pairs:
             print(anti_pair)
             pairs.remove(anti_pair)
+print(len(pairs))
 
 matches = {0: 0}
 # Apply Greedy
 while len(matches) > max(matches.values()):
     matches = {}
+    
+    print(f'Tallies: {len(matches)} remaining')
     for pair in pairs:
         k = pair[0]
         v = pair[1]
@@ -38,12 +43,15 @@ while len(matches) > max(matches.values()):
             matches[k] = 0
         matches[k] += 1
 
-    print(matches)
+    print('Find least paired')
     k = min(matches, key=matches.get)
     v = matches[k]
     floor = v
+    print('Floor: {floor}')
     while matches[k] == floor:
+        print(f'del matches[{k}]= {matches[k]}')
         del matches[k]
         k = min(matches, key=matches.get)
         v = matches[k]
-print(matches)
+
+print(matches.keys())
